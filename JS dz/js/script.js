69fd9025ae3ea703f339e537c107a3cbdee0b4fb
Fedosova-lyzhankova-Ylia <liyiza@yandex.tu>
2022-08-15 17:05:20 +0300

    dz 1-5

--- JS dz/js/script.js
+++ JS dz/js/script.js
@@ -1,23 +1,68 @@
 "use strict";
 //задание 1
-let result = prompt("Введите текст", "Введите текст");
+const result = prompt("Введите текст", "Введите текст");
 console.log(result);
 alert(`${result}`);
 
 //задание 2
 alert("Введи три числа и получишь их сумму");
-let algoritmH1 = +prompt("введите первое число");
-let algoritmH2 = +prompt("введите второе число");
-let algoritmH3 = +prompt("введите третье  число");
+const algoritmH1 = +prompt("введите первое число");
+const algoritmH2 = +prompt("введите второе число");
+const algoritmH3 = +prompt("введите третье  число");
 console.log(algoritmH1 + algoritmH2 + algoritmH3);
 alert(`СУММА ${algoritmH1 + algoritmH2 + algoritmH3}`);
 
 //задание 3
-let nameN = prompt("Введите ваше имя", "ваше имя");
-let ageN = +prompt("Сколько Вам лет?", "введите возрат в цифрах");
-if (ageN >='18') {
-    alert(`ДОБРО ПОЖАЛОВАТЬ ${nameN}`);
-  } else {
-      alert(`Простите,${nameN}доступ ЗАКРЫТ`);
-  }
+const nameN = prompt("Введите ваше имя", "ваше имя");
+const ageN = +prompt("Сколько Вам лет?", "введите возрат в цифрах");
+if (ageN >= 18) {
+  alert(`ДОБРО ПОЖАЛОВАТЬ ${nameN}`);
+} else {
+  alert(`Простите,${nameN}доступ ЗАКРЫТ`);
+}
 
+//задание 4
+let nameBlok = confirm("Хотите ввести название блока?");
+if (nameBlok == true) {
+  nameBlok = prompt("Введите название блока");
+} else {
+  console.log(NaN);
+}
+let nameElement = confirm("Хотите ввести название элемента?");
+if (nameElement == true) {
+  nameElement = prompt("Введите название элемента");
+} else {
+  console.log(NaN);
+}
+let nameMod = confirm("Хотите ввести название модификатора?");
+if (nameMod == true) {
+  nameMod = prompt("Введите название модификатора");
+} else {
+  console.log(NaN);
+}
+console.log(`${nameBlok} ${nameElement} ${nameMod}`);
+alert(`ИМЯ ${nameBlok}  ${nameElement} ${nameMod}`);
+
+//задание 5
+alert("Создай пример сам и получи ответ");
+const a = +prompt("введите первое число");
+const b = +prompt("введите второе число");
+const arifmet = prompt("введите нужную арифметическую операцию на выбор /,+,-,*");
+if ((arifmet == '/')) {
+  console.log(a / b);
+  alert(` ${a / b}`);
+}
+if ((arifmet == '+')) {
+    console.log(a + b);
+    alert(` ${a + b}`);
+  }
+  if ((arifmet == '*')) {
+    console.log(a * b);
+    alert(` ${a * b}`);
+  }
+  if ((arifmet == '-')) {
+    console.log(a - b);
+    alert(` ${a - b}`);
+  } else {
+    console.log(NaN);
+  }
